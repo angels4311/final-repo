dda0370a2594446fd6db68bae66fcb6fd9f784aa
angels4311 <8asoto2@stpatrick.org>
2016-12-14 11:31:10 -0600

    day3

--- cart.js
+++ cart.js
@@ -1,6 +1,13 @@
 class Cart{
   //What is the first part of every class? Type it below.
+constructor(i,q){
+    this.itemList = i;
+    this.itemQuantity = q;
 
+
+
+
+}
 
 
   //Type the instance functions below this comment.
---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,mp,st){
+    this.name = n;
+    this.price = p;
+    this.shoppingTime = st;
+}
+
+
+}
+
+//Create your three test items below this comment.
+let mygift = new item ("tv",250,8);
+let yourgift = new item("galaxy", 600,7);
+let hisgift = new item("sweater",200,5);
